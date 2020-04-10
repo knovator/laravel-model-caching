13b1e80c05fd3005464e661681d67de8d407c701
priyankapatel@knovator.in <admin@123>
2020-04-10 16:02:45 +0530

    added additional files for relation

---
+++ src/Console/Commands/ChangePathCommand.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace Knovators\LaravelModelCaching\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\File;
+
+
+/**
+ * Class ChangePathCommand
+ * @package App\Console\Commands
+ */
+class ChangePathCommand extends Command
+{
+
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'fix:caching_mongodb';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+    protected $mongoModel = 'Jenssegers\Mongodb\Eloquent\Model';
+    protected $mongoRelation = 'Jenssegers\Mongodb\Relations';
+    protected $mongoBuilder = 'Jenssegers\Mongodb\Query\Builder';
+    protected $laravelModel = 'Illuminate\Database\Eloquent\Model';
+    protected $laravelRelation = 'Illuminate\Database\Eloquent\Model';
+    protected $laravelBuilder = 'Illuminate\Database\Eloquent\Builder';
+    protected $paths = [
+        'vendor/knovators/laravel-model-caching/src',
+    ];
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct() {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle() {
+        foreach ($this->paths as $path) {
+            if (File::isDirectory($path)) {
+                $files = File::allfiles($path);
+                foreach ($files as $filename) {
+                    $str = file_get_contents($filename);
+                    $str = str_replace($this->laravelModel, $this->mongoModel, $str);
+                    $str = str_replace($this->laravelBuilder, $this->mongoBuilder, $str);
+                    $str = str_replace($this->laravelRelation, $this->mongoRelation, $str);
+                    file_put_contents($filename, $str);
+
+                }
+            }
+        }
+    }
+}
--- src/Providers/Service.php
+++ src/Providers/Service.php
@@ -1,29 +1,34 @@
 <?php namespace Knovators\LaravelModelCaching\Providers;
 
+use Knovators\LaravelModelCaching\Console\Commands\ChangePathCommand;
 use Knovators\LaravelModelCaching\Console\Commands\Clear;
 use Knovators\LaravelModelCaching\Console\Commands\Publish;
 use Knovators\LaravelModelCaching\Helper;
 use Illuminate\Support\ServiceProvider;
 
+/**
+ * Class Service
+ * @package Knovators\LaravelModelCaching\Providers
+ */
 class Service extends ServiceProvider
 {
+
     protected $defer = false;
 
-    public function boot()
-    {
+    public function boot() {
         $configPath = __DIR__ . '/../../config/laravel-model-caching.php';
         $this->mergeConfigFrom($configPath, 'laravel-model-caching');
         $this->commands([
             Clear::class,
             Publish::class,
+            ChangePathCommand::class
         ]);
         $this->publishes([
             $configPath => config_path('laravel-model-caching.php'),
         ], "config");
     }
 
-    public function register()
-    {
+    public function register() {
         $this->app->bind("model-cache", Helper::class);
     }
 }
